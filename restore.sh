4890ed69ee50490b9f3fc37afd7f6e7136ebf1a9
edwinrivera1995 <edwinrivera19955@gmail.com>
2020-03-30 23:50:42 +0000

    fix: Update README.md with correct ports(#485)

--- restore.sh
+++ restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set start and end date
-START_DATE="2021-05-7"  # YYYY-MM-DD
-END_DATE="2021-10-24"    # YYYY-MM-DD
+START_DATE="2020-03-7"  # YYYY-MM-DD
+END_DATE="2020-10-24"    # YYYY-MM-DD
 
 # Min and max commits per selected day
 MIN_COMMITS=1
