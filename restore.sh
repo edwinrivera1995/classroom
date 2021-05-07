b28da476f5d7384465451e4dba085ef873661aaa
edwinrivera1995 <edwinrivera19955@gmail.com>
2021-05-07 10:12:37 +0000

    feat: disable umami analytics on localhost for dashboard and classroomio-com (#505)

--- restore.sh
+++ restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set start and end date
-START_DATE="2022-06-7"  # YYYY-MM-DD
-END_DATE="2022-12-24"    # YYYY-MM-DD
+START_DATE="2021-05-7"  # YYYY-MM-DD
+END_DATE="2021-10-24"    # YYYY-MM-DD
 
 # Min and max commits per selected day
 MIN_COMMITS=1
